383b27c5a943459be92b0f2e6a190480bcdfe94a
Daniil Hovorun <hovorun.d@gmail.com>
2019-12-25 22:46:12 +0200

    Functioning Lists of Owners and Vets added

--- pet-clinic-data/src/main/java/com/hovorun/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/hovorun/sfgpetclinic/services/VetService.java
@@ -2,6 +2,6 @@ package com.hovorun.sfgpetclinic.services;
 
 import com.hovorun.sfgpetclinic.model.Vet;
 
-public interface VetService extends CrudService<Vet, Long> {
+public interface VetService extends CrudService <Vet, Long> {
 
 }
